543da90da701676f0a2a6c698b8b190b3b735520
Sebastian <sebastian_koenig@cotyinc.com>
2020-07-07 14:47:03 +0200

    2 finished

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -5,8 +5,10 @@
 <script>
 export default {
     name: 'BookList',
-    data (){
-        title: 'All Books'
+    data () {
+        return {
+            title: 'All Books'
+        }
     }
 }
 </script>
